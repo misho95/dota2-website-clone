a37e9057242d3864206ebc64c7ac1fe2d2af5384
Misho <chapidze.misho95@gmail.com>
2024-04-01 19:22:35 +0400

    s

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 function App() {
   return (
-    <div className="w-[100vw] h-[100vh] bg-red-500">
-      <video autoPlay preload="auto" loop playsInline controls>
+    <div className="w-[100vw] h-full relative">
+      <video autoPlay preload="auto" loop playsInline muted>
         <source type="video/webm" src={"src/assets/dota.webm"} />
+        <source type="video/webm" src={"src/assets/dota.mp4"} />
       </video>
+      <div className="w-[100vw] h-[200px] absolute bottom-0 left-0 bg-gradient-to-b from-transparent via-black-70 to-black" />
     </div>
   );
 }
