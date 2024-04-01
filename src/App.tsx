45b0f409eabb79bfd207db7773614d1ca8827624
Misho <chapidze.misho95@gmail.com>
2024-04-01 17:58:48 +0400

    s

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 function App() {
-  return <div>hello</div>;
+  return (
+    <div className="w-[100vw] h-[100vh] bg-red-500">
+      <video autoPlay preload="auto" loop playsInline controls>
+        <source type="video/webm" src={"src/assets/dota.webm"} />
+      </video>
+    </div>
+  );
 }
 
 export default App;
